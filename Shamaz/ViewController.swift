bf0c4bbd5611bd629e88176011acb5fa023998df
geovannyllerena <geovanny_llerena@hotmail.com>
2019-04-10 12:23:54 +0100

    Initial Commit

---
+++ Shamaz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Shamaz
+//
+//  Created by geovanny llerena on 10/04/2019.
+//  Copyright © 2019 geovanny llerena. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
