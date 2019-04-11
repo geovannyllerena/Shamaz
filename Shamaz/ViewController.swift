18146b8df1011669603d61f12bb640e936ba4230
geovannyllerena <geovanny_llerena@hotmail.com>
2019-04-11 20:46:32 +0100

    View update

--- Shamaz/ViewController.swift
+++ Shamaz/ViewController.swift
@@ -10,11 +10,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+   
+    @IBOutlet weak var firstText: UILabel!
+    
+    
+    @IBAction func pastAction() {
+       //Initialize Sting and Array
+        let mainSentence: String = "what did you do "
+        var pastSentence: Array = ["at your birthday 3 years ago?","2 days ago?","3 months ago?","at uni a week ago?","at work 3 days ago?","at home a day ago?"]
+       //Generate the ramdom index to use to form the snetence
+        let pastSentenceIndex: Int = Int(arc4random_uniform(UInt32(pastSentence.count)))
+        let chosenPastSentence = pastSentence[pastSentenceIndex]
+        
+        firstText.text = mainSentence + chosenPastSentence
     }
-
-
+    
+    @IBAction func futurePlans() {
+        let mainSentence: String = "What are you planing to do "
+        var futureSentences: Array = ["next Monday at work?","next weekend?","next summer?","next year?","next holidays?","next Friday night?"]
+        let futureSentenceIndex: Int = Int(arc4random_uniform(UInt32(futureSentences.count)))
+        let chosenFutureSentence: String = futureSentences[futureSentenceIndex]
+        
+        firstText.text = mainSentence + chosenFutureSentence
+        }
+    
+   
+    @IBAction func nextPlayer() {
+        //creates the random number in the label
+        let randomNumber: Int = Int.random(in: 1 ..< 11)
+        //let randomNumber: Int = Int(arc4random_uniform(UInt32(numberOfPlayers)))
+        //
+        firstText.text = String (randomNumber)
+        
+    
+    }
+    
+    
+    
 }
 
